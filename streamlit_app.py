eced98f8e190e9207fe092d91f0d4340f824bdbe
serhii9227 <160024560+serhii9227@users.noreply.github.com>
2024-07-31 11:15:27 +0000

    Change title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Home work #9")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Goal: Learn and use elements and methods of streamlit in order to ."
 )
